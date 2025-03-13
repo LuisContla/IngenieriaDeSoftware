2b18182937ce44ea84aa5aeb05aaead0b5b17de2
Luis Contla <luis.contla.mota30@gmail.com>
2025-03-12 19:34:35 -0600

    Práctica 1 - Vista Admin
    Se añadió a la Práctica 1 las vistas de administrador y se modificaron las vistas de Usuario normal (Home)

---
+++ Practica1/Práctica 1/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Práctica_1.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Books()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+    }
+}
