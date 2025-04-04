208ae71ca32c74710b342d08d6bd0fd8aa21dc5b
Luis Contla <luis.contla.mota30@gmail.com>
2025-04-04 17:09:25 -0600

    Práctica - Proyecto Único
    Se eliminó la carpeta de Práctica 2. Se trabajará la práctica sobre el mismo proyecto. Se renombró la carpeta Práctica 1 a Práctica, donde se almacenará el proyecto

--- Práctica2/Práctica 2/Models/LoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Práctica_2.Models
-{
-    public class LoginViewModel
-    {
-        public string Usuario { get; set; }
-        public string Contraseña { get; set; }
-    }
-}
--- Práctica2/Práctica 2/Models/RegisterViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Práctica_2.Models
-{
-    public class RegisterViewModel
-    {
-        public string Usuario { get; set; }
-        public string Correo { get; set; }
-        public string Contraseña { get; set; }
-    }
-}
